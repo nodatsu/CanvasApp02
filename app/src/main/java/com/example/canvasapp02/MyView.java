c416880ded72cfd5ef94b56efdba143582b416e4
H.Tsunoda <tsunoda@aomori-u.ac.jp>
2019-02-23 17:38:23 +0900

    三角形(パス)を描いた

--- app/src/main/java/com/example/canvasapp02/MyView.java
+++ app/src/main/java/com/example/canvasapp02/MyView.java
@@ -4,16 +4,19 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.view.View;
 
 public class MyView extends View {
     private Paint paint;
+    private Path path;
 
     // コンストラクタ(初期設定)
     public MyView(Context con) {
         super(con);
 
         paint = new Paint();
+        path = new Path();
     }
 
     // 描画の処理
@@ -41,6 +44,24 @@ public class MyView extends View {
         paint.setStrokeWidth(10);
         canvas.drawLine(0, 0, canvas.getWidth(), canvas.getHeight(), paint);
         canvas.drawLine(0, canvas.getHeight(), canvas.getWidth(), 0, paint);
+
+        // 三角形(パス)
+        paint.setColor(Color.BLACK);
+        paint.setStyle(Paint.Style.STROKE);
+        path.moveTo(500, 500);
+        path.lineTo(700, 900);
+        path.lineTo(400, 800);
+        path.lineTo(500, 500);
+        canvas.drawPath(path, paint);
+
+        paint.setColor(Color.BLUE);
+        path.reset();
+        path.moveTo(300, 1000);
+        path.lineTo(700, 1100);
+        path.lineTo(200, 1200);
+        path.lineTo(300, 1000);
+        canvas.drawPath(path, paint);
+
     }
 
 }
