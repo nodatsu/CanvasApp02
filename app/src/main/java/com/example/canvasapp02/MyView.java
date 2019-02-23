6edd55f39d5624876e32c719df82d061768a76c7
H.Tsunoda <tsunoda@aomori-u.ac.jp>
2019-02-23 15:54:07 +0900

    キャンバスを使った表示クラスを追加

---
+++ app/src/main/java/com/example/canvasapp02/MyView.java
@@ -0,0 +1,15 @@
+package com.example.canvasapp02;
+
+import android.content.Context;
+import android.view.View;
+
+public class MyView extends View {
+
+
+    // コンストラクタ(初期設定)
+    public MyView(Context con) {
+        super(con);
+    }
+
+
+}
