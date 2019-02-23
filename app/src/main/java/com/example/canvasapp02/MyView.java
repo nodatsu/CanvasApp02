539b242a0d01870dbcc4e9c8f39eeb79105171ed
H.Tsunoda <tsunoda@aomori-u.ac.jp>
2019-02-23 16:58:57 +0900

    円を描いた

--- app/src/main/java/com/example/canvasapp02/MyView.java
+++ app/src/main/java/com/example/canvasapp02/MyView.java
@@ -3,14 +3,17 @@ package com.example.canvasapp02;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.view.View;
 
 public class MyView extends View {
-
+    private Paint paint;
 
     // コンストラクタ(初期設定)
     public MyView(Context con) {
         super(con);
+
+        paint = new Paint();
     }
 
     // 描画の処理
@@ -19,6 +22,14 @@ public class MyView extends View {
 
         // 背景
         canvas.drawColor(Color.GRAY);
+
+        // 円
+        paint.setColor(Color.RED);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(20);
+        canvas.drawCircle(300, 500, 50, paint);
+        paint.setColor(Color.GREEN);
+        canvas.drawCircle(300, 600, 50, paint);
 
     }
 
